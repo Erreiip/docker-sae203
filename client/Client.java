845ef22a7af66ec2c6d7e6ebce2710563d5206f8
Pierre LE MEUR <pierre.lemeur72@gmail.com>
2022-05-24 17:45:14 +0200

    réparation client

--- client/Client.java
+++ client/Client.java
@@ -51,7 +51,7 @@ public class Client extends Thread{
         String pseudo = "";
 
         try {
-            Scanner sc = new Scanner ( new FileReader( "client/infos/pseudo.txt" ) );
+            Scanner sc = new Scanner ( new FileReader( "./infos/pseudo.txt" ) );
 
             pseudo = sc.nextLine();
 
@@ -66,7 +66,7 @@ public class Client extends Thread{
 
         try {
 
-            PrintWriter pr = new PrintWriter( "./client/infos/pseudo.txt");
+            PrintWriter pr = new PrintWriter( "./infos/pseudo.txt");
             pr.print(pseudo);
             pr.close();
 
@@ -85,7 +85,7 @@ public class Client extends Thread{
         try {
             String nbConexions = "";
 
-            Scanner sc = new Scanner ( new FileReader( "./client/infos/nombreConnexions.txt" ));
+            Scanner sc = new Scanner ( new FileReader( "./infos/nombreConnexions.txt" ));
 
             if ( sc.hasNextLine() ) 
                 nbConexions  = sc.nextLine();
@@ -93,7 +93,7 @@ public class Client extends Thread{
 
             if ( nbConexions.equals("0") ) {
                 
-                PrintWriter pr = new PrintWriter( "./client/infos/nombreConnexions.txt" );
+                PrintWriter pr = new PrintWriter( "./infos/nombreConnexions.txt" );
                 pr.print("1");
                 pr.close();
                 sc.close();
