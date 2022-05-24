72a2de7e77e92483a0a6c591cf8a4aea43d8f776
Pierre LE MEUR <pierre.lemeur72@gmail.com>
2022-05-24 18:40:34 +0200

    rrerepatch

---
+++ class/Test.java
@@ -0,0 +1,17 @@
+import serveur.GuildManager;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        File file = new File("./guilds/test.json");
+        System.out.println(file.toString());
+        System.out.println(file.exists());
+
+        GuildManager.creerServeur("test", "test");
+    }
+}
